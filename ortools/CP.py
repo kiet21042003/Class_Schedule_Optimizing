1bd7f407ce9ed97548197ac0c315de96c8009e7e
Đặng Kiều Trinh <93564987+cincin-2399@users.noreply.github.com>
2023-02-07 23:49:36 +0700

    Update CP.py

--- ortools/CP.py
+++ ortools/CP.py
@@ -1,35 +1,6 @@
 from ortools.sat.python import cp_model
 import time
 
-'''
-class VarArraySolutionPrinter(cp_model.CpSolverSolutionCallback):
-	# print intermediate solution
-	def __init__(self,vars, N, M, c, s, g, limit = 1):
-		cp_model.CpSolverSolutionCallback.__init__(self) 
-		self.__vars = vars 
-		self.__solution_count = 0
-		self.__N = N
-		self.__M = M
-		self.__c = c
-		self.__s = s
-		self.__g = g
-		self.__limit = limit
-	def on_solution_callback(self):
-		self.__solution_count += 1
-		print(f'\nSolution {self.__solution_count}:')
-		for n in range(self.__N):
-			for i in range(5):
-				for j in range(12):
-					for m in range(self.__M):
-						if self.Value(self.__vars[n][i][j][m]):
-							print(f'\tClass {n} has lesson in shift {j} of day {i} at room {m} having {self.__c[m]} slots, has {self.__s[n]} students and is taught by teacher {self.__g[n]}')
-		
-		if self.__solution_count >= self.__limit:
-			self.StopSearch()
-	def solution_count(self):
-		return self.__solution_count
-'''
-
 def Time(d):
     days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
     return days[d]
@@ -80,12 +51,6 @@ def CP(filename):
             for k in range(12):
                 for r in range(M):
                     model.Add(s[i] * x[i,d,k,r] <= c[r])
-                    
-    ## The total number of times that class i is scheduled in the timetable must be less than the number of lessons for class i (t[i]). 
-    ## This ensures that the class is not scheduled more times than it should be
-    '''for i in range(N):
-        model.Add(sum(x[i,d,k,r] \
-             for d in range(5) for k in range(12) for r in range(M)) <= t[i])'''
 
     ## There can be only be one class at one room in a moment
     for d in range(5):
@@ -102,7 +67,7 @@ def CP(filename):
         model.Add(sum(x[i,d,k,r] for d in range(5) for k in range(12) for r in range(M)) == t[i]).OnlyEnforceIf(c)
         model.Add(sum(x[i,d,k,r] for d in range(5) for k in range(12) for r in range(M)) <= t[i]).OnlyEnforceIf(c.Not())
 
-    '''Total_Shifts = {}
+    Total_Shifts = {}
     for i in range(N):
         for d in range(5):
             Total_Shifts[i, d] = model.NewIntVar(0, t[i], f'Total_Shifts[{i}, {d}]')
@@ -115,11 +80,11 @@ def CP(filename):
     Least_Shifts_Day = {}
     for i in range(N):
         Least_Shifts_Day[i] = model.NewIntVar(0, t[i], f'Least_Shifts_Day[{i}]')
-        model.AddMinEquality(Least_Shifts_Day[i], [Total_Shifts[i, d] for d in range(5)])'''
+        model.AddMinEquality(Least_Shifts_Day[i], [Total_Shifts[i, d] for d in range(5)])
     
     # Objective function
 
-    model.Maximize(sum(y[i] for i in range(N)))
+    model.Maximize(sum(y[i] for i in range(N)) - sum(Most_Shifts_Day[i] - Least_Shifts_Day[i] for i in range(N)))
 
     # Create solver
     solver = cp_model.CpSolver()
@@ -140,9 +105,7 @@ def CP(filename):
         print("No solution")
 
 if __name__ == '__main__':
-    from data_gen import *
     filename = "data.txt"
-   #gen(filename, 100, 50)
     t1 = time.time()
     CP(filename)
     t2 = time.time()
